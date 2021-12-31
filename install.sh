e8c8e30bfc2ceca7d7b644a17e148f1fb953ca6e
danielronalds <nzdanron@gmail.com>
2021-12-31 14:38:57 +1300

    First test

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ echo $sudopassowrd | sudo -S echo ""
 echo "[ Setup ] Adding speed-ricer ppa"
 sudo add-apt-repository -y ppa:kgilmer/speed-ricer
 sudo apt update
+
 echo "[ Setup ] Installing required packages"
-sudo apt install i3-gaps polybar picom rofi vim python3-pip git nitrogen 
+sudo apt install fish i3-gaps polybar picom rofi vim python3 python3-pip git nitrogen 
 pip3 install i3ipc
 pip3 install keyboard
